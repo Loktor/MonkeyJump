33812e9ec0124b1494ccb0515e8342506c6721e0
Loktor <e.david@gmx.at>
2012-11-06 11:16:03 +0100

    Signed-off-by: Loktor <e.david@gmx.at>

--- Monkey Jump/Monkey Jump/Screens/LoadingScreen.cs
+++ Monkey Jump/Monkey Jump/Screens/LoadingScreen.cs
@@ -144,7 +144,7 @@ namespace Monkey_Jump
                 SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
                 SpriteFont font = ScreenManager.Font;
 
-                const string message = "Loading...";
+                const string message = "Loading Bananas...";
 
                 // Center the text in the viewport.
                 Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
